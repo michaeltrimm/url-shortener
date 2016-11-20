224ea28889e6c6a8e3c2e1868e2f17fb016c4704
Michael Trimm <michael@michaeltrimm.com>
2016-11-20 16:23:31 -0500

    Added CORS verification to protect the application

--- index.php
+++ index.php
@@ -48,6 +48,19 @@ $app = new \Slim\App([
   ]
 ]);
 
+# CORS
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+  return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+  $response = $next($req, $res);
+  return $response
+          ->withHeader('Access-Control-Allow-Origin', 'https://'.$DOMAIN)
+          ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+          ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 $app->post('/new', function($request, $response, $args) use ($pdo) {
   $data = $request->getParsedBody();
   if(!isset($data['url']) || strlen($data['url']) <= 5){
